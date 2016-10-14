ae53d864530f84ce4d1503839e6c55eddc726fc8
Stephen <sking@mail.greenriver.edu>
2016-10-13 18:02:33 -0700

    Stephen Test Directory

---
+++ public_html/stephen/controllers/validationScript.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+/**
+ * Author: Chris Barbour
+ *
+ * Driver for loginValidation class
+ */
+require('../loginValidation.php');
+$_SESSION['loggedIn'] = 'false';
+
+//if call is sent by post
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $validator = new LoginValidation($_POST['userName'], $_POST['password']);
+    $response = $validator->validateLogin();
+
+    //set session variables
+    if($response['valid']==='yes'){
+        $_SESSION['user'] = $_POST['userName'];
+        $_SESSION['loggedIn'] = 'true';
+    }
+    header('content-type:application/json');
+    echo json_encode($response);
+}
+?>
---
+++ public_html/stephen/views/OptionsView.php
@@ -0,0 +1,42 @@
+<?php
+    include('/home/casino/public_html/modals/OptionsModal.php');
+
+    $optionsModal = new OptionsModal(1);
+    $options = $optionsModal->getPromotionSettings();
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Settings View</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+         dialog = $("#settings").dialog({
+                autoOpen: true,
+                height: 400,
+                width: 350,
+                modal: true,
+                buttons: {
+                    Submit: function () {
+                        //submit changes to db through options modal class
+                        dialog.dialog('close');
+                    }
+                }
+            });
+        } );
+    </script>
+</head>
+<body>
+    <div id="settings" title="Settings">
+        <?php
+            foreach ($options as $setting => $value){
+                echo $setting . " = " . $value . "<br>";
+            }
+        ?>
+    </div>
+</body>
+
