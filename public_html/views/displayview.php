4777339038cfa13e7b34e8b8274a4dcccdcd5097
alexOnorati <aonorati@mail.greenriver.edu>
2016-11-03 20:23:08 -0700

    test commit

--- public_html/views/displayview.php
+++ public_html/views/displayview.php
@@ -1,27 +1,64 @@
-<div id="boxes">
-    <?php $displays = $box->getAllBoxesWithCasinoId(1);
-    foreach ($displays as $display){?>
-        <div class="display-outer">
-            <div class="display-body container">
-                <div class="display-header row">
-                    <div class="col-md-4"><h3 id="name" class="header-text display-friendly-name display-font">
-                            <?php echo $display->getName();?></h3></div>
-                    <div class="col-md-4"><h3 id="serial" class="header-text display-font">
-                            <?php echo $display->getSerial(); ?></h3></div>
-                    <div class="col-md-4 edit-display-div">
-                        <button type="button" class="btn btn-info btn-lg edit-display-btn">EDIT</button>
-                    </div>
-                </div>
-                <hr class="display">
-                <?php
-                $promotions = $display->getPromotions();
-                foreach($promotions as $promotion){
-                    ?>
-                    <div class="promotion-preview-body <?php echo $promotion; //$row['promo_id'];?>">
-                        <img class="promotion-preview-icon" src="dependencies/images/<?php echo "pointsgt.png"; //$row['promo_image']?>">
-                    </div>
-                <?php } ?>
-            </div>
+<div class="display-outer">
+    <div class="display-body container">
+        <div class="display-header row">
+            <div class="col-md-4"><h3 class="header-text display-friendly-name display-font">Front Door</h3></div>
+            <div class="col-md-4"><h3 class="header-text display-font">GACLAK-01</h3></div>
+            <div class="col-md-4 edit-display-div"><button type="button" class="btn btn-info btn-lg edit-display-btn">EDIT</button></div>
         </div>
-    <?php } ?>
+        <hr class="display">
+
+        <?php
+            for($x = 0; $x < 20; $x++){
+        ?>
+        <div class="promotion-preview-body <?php echo "test1"; //$row['promo_id'];?>">
+            <img class="promotion-preview-icon" src="dependencies/images/<?php echo "pointsgt.png"; //$row['promo_image']?>">
+        </div>
+
+
+        <?php } ?>
+    </div>
+</div>
+
+<!--These are just tests for sizing-->
+
+<div class="display-outer">
+    <div class="display-body container">
+        <div class="display-header row">
+            <div class="col-md-4"><h3 class="header-text display-friendly-name display-font">Front Door</h3></div>
+            <div class="col-md-4"><h3 class="header-text display-font">GACLAK-01</h3></div>
+            <div class="col-md-4 edit-display-div"><button type="button" class="btn btn-info btn-lg edit-display-btn">EDIT</button></div>
+        </div>
+        <hr class="display">
+
+        <?php
+        for($x = 0; $x < 20; $x++){
+            ?>
+            <div class="promotion-preview-body <?php echo "test1"; //$row['promo_id'];?>">
+                <img class="promotion-preview-icon" src="dependencies/images/<?php echo "pointsgt.png"; //$row['promo_image']?>">
+            </div>
+
+
+        <?php } ?>
+    </div>
+</div>
+
+<div class="display-outer">
+    <div class="display-body container">
+        <div class="display-header row">
+            <div class="col-md-4"><h3 class="header-text display-friendly-name display-font">Front Door</h3></div>
+            <div class="col-md-4"><h3 class="header-text display-font">GACLAK-01</h3></div>
+            <div class="col-md-4 edit-display-div"><button type="button" class="btn btn-info btn-lg edit-display-btn">EDIT</button></div>
+        </div>
+        <hr class="display">
+
+        <?php
+        for($x = 0; $x < 20; $x++){
+            ?>
+            <div class="promotion-preview-body <?php echo "test"; //$row['promo_id'];?>">
+                <img class="promotion-preview-icon" src="dependencies/images/<?php echo "pointsgt.png"; //$row['promo_image']?>">
+            </div>
+
+
+        <?php } ?>
+    </div>
 </div>
