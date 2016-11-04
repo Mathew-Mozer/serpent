5275c1997f5e14b06d117bafbafd43192bbc544f
alexOnorati <aonorati@mail.greenriver.edu>
2016-11-03 19:23:47 -0700

    added kick for cash promotion

---
+++ public_html/views/addpromotionviews/kickforcashview.php
@@ -0,0 +1,9 @@
+<br>
+<label>Target Number</label>
+<br>
+<input id="target-number" name="target-number" type="number"></input>
+<br>
+<label>Cash Prize </label>
+<br>
+<input id="cash-prize" name="cash-prize" type = "number"></input>
+<script src="dependencies/js/promotion/kickforcash.js"></script>
