960e80317cd0589231c62fd8409be922fa834521
christopherbarbour <cbarbour86@outlook.com>
2016-11-08 12:58:17 -0800

    updated unassignedbox tile css

--- public_html/views/unassignedboxview.php
+++ public_html/views/unassignedboxview.php
@@ -1,6 +1,6 @@
 
 <div class="display-outer" id="unassigned-boxes">
-    <h3> Unassigned Boxes </h3>
+    <h3> Unassigned Displays </h3>
     <?php
         //uses default casino id which is 0
         $unassigned = new CasinoBoxes($dbcon->read_database(), 0);
