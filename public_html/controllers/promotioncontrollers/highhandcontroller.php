6a214d9f86fd7c59aadb1f2419befc92cdbad2cb
christopherbarbour <cbarbour86@outlook.com>
2016-11-23 05:07:12 -0800

    created template system for high hand promotion

--- public_html/controllers/promotioncontrollers/highhandcontroller.php
+++ public_html/controllers/promotioncontrollers/highhandcontroller.php
@@ -9,10 +9,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($_POST['action'] == 'add') {
         $promotion = new PromotionModel($conn->insert_database());
         $highHand = new HighHandModel($conn->insert_database());
-        $addPromotion = $promotion->addPromotion($_POST['promotionId'], $_POST['casinoId'], $_POST['sceneType']);
+        $addPromotion = 0;
+        if($_POST['isTemplate'] != 'true') {
+            $addPromotion = $promotion->addPromotion($_POST['promotionId'], $_POST['casinoId'], $_POST['sceneType']);
+        }
         $highHand->addHighHand($addPromotion, $_POST['titleMessage'], $_POST['useJoker'],
             $_POST['highHandGold'], $_POST['hornTimer'], $_POST['payoutValue'], $_POST['sessionTimer'],
-            $_POST['multipleHands']);
+            $_POST['multipleHands'], $_POST['isTemplate']);
 
         $response = array();
         $response['image'] = $promotion->getPromotionImageByPromotionType($_POST['promotionId']);
@@ -23,9 +26,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else if ($_POST['action'] == 'read') {
         $highHand = new HighHandModel($conn->read_database());
         $response = $highHand->getHighHand($_POST['promotionId']);
-
         header('content-type:application/json');
-
+        echo json_encode($response);
+    } else if ($_POST['action'] == 'template'){
+        $highHand = new HighHandModel($conn->read_database());
+        $response = $highHand->getTemplate();
+        header('content-type:application/json');
         echo json_encode($response);
     }
 }
